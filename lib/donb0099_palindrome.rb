a0db2f2d2791d568c529d1c8f655215b15cf4c4d
donb0099 <don.blaskowsky@earthlink.net>
2020-12-09 00:00:19 +0000

    Initialize repository

---
+++ lib/donb0099_palindrome.rb
@@ -0,0 +1,6 @@
+require "donb0099_palindrome/version"
+
+module Donb0099Palindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/donb0099_palindrome/version.rb
@@ -0,0 +1,3 @@
+module Donb0099Palindrome
+  VERSION = "0.1.0"
+end
